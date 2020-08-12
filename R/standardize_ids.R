05edd6fc673c91789ace7cc567f27511f24cc018
jrf1111 <39165225+jrf1111@users.noreply.github.com>
2020-08-12 16:21:26 -0500

    Reformat args

--- R/standardize_ids.R
+++ R/standardize_ids.R
@@ -4,7 +4,7 @@
 #' @param length a scalar integer indicating how long the result should be.  Default is 7.
 #'
 #' @description Standardizes the length of IDs (or other variables) by adding leading zeros.
-#' 
+#'
 #' @return a character vector of IDs with leading zeros added
 #'
 #'
@@ -13,22 +13,22 @@ standardize_ids = function(id, length = 7){
 	if(!is.numeric(id)){
 		message("standardize_ids needs to be fed a numeric input (or one that can be coerced to numeric) to work properly")
 	}
-	
+
 	na_pre = sum(is.na(id))
 	res = as.numeric(id)
 	na_post = sum(is.na(res))
 
 	if(na_pre == na_post){
 		message("Treating input argument 'id' as numeric")
-		res = formatC(res, width = length, flag="0", format="d")
+		res = formatC(res, width = length, flag = "0", format = "d")
 		return(res)
 	}
-	
+
 	if(na_pre != na_post){
 		stop("Input argument 'id' could not safely be coerced to numeric. Returning input unchanged.")
 		return(id)
 	}
-	
-	
-	
+
+
+
 }
