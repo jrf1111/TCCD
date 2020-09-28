a630026e0f00fb1fe246ff4e491d292450b49e48
jrf1111 <39165225+jrf1111@users.noreply.github.com>
2020-09-28 09:44:42 -0500

    add theme_tcc()

--- R/TCC_color_scales.R
+++ R/TCC_color_scales.R
@@ -95,3 +95,35 @@ scale_fill_tccd = function(palette = "main", discrete = TRUE, reverse = FALSE, .
 		ggplot2:::scale_color_gradientn(colours = pal(256), ...)
 	}
 }
+
+
+
+
+
+theme_tcc = function(rotate_x_labs = FALSE,
+										 base_size = 11,
+										 base_family = "",
+										 base_line_size = base_size/22,
+										 base_rect_size = base_size/22)
+{
+	res = theme_grey(base_size = base_size,
+									 base_family = base_family,
+									 base_line_size = base_line_size,
+									 base_rect_size = base_rect_size) %+replace%
+		theme(panel.background = element_rect(fill = "white", colour = NA),
+					panel.border = element_rect(fill = NA, colour = "grey20"),
+					panel.grid = element_line(colour = "grey92"),
+					panel.grid.minor = element_line(size = rel(0.5)),
+					strip.background = element_rect(fill = "grey85", colour = "grey20"),
+					legend.key = element_rect(fill = "white", colour = NA),
+					plot.caption.position = "plot",
+					complete = TRUE)
+
+	if(rotate_x_labs){
+		res = res %+replace%
+			theme(axis.text.x = element_text(angle = -90, vjust = 0.5, hjust = 0))
+	}
+
+
+}
+
