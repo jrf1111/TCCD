54a2f0c9a944be62cfa62672c51c54238b5a3ffa
jrf1111 <39165225+jrf1111@users.noreply.github.com>
2020-10-22 13:39:21 -0500

    Add factor_term()

--- R/Term_to_numeric_value.R
+++ R/Term_to_numeric_value.R
@@ -11,7 +11,7 @@ term_num = function(TERM_ID){
 
 	TERM_ID = tibble::tibble(TERM_ID = TERM_ID)
 
-	mtable = tibble::tibble(TERM_ID =  paste0(rep(2004:2050, each=3), c("SP", "SU", "FL")),
+	mtable = tibble::tibble(TERM_ID =  paste0(rep(2004:2050, each = 3), c("SP", "SU", "FL")),
 													term_num = 1:length(TERM_ID)
 													)
 
@@ -43,4 +43,52 @@ Term_name = function(TERM_ID){
 	res = gsub(".*?SU", "Summer", res)
 	res
 
+}
+
+
+
+
+
+#' Get factor-ized terms
+#'
+#' @description `factor_term()` takes a Term column/variable and returns it as a factor for sorting purposes.
+#'
+#' @param Term A variable containing academic terms.
+#' @param drop Character. How should term levels not seen in the data be handled? Default is `drop = "outside"`, which removes term levels outside of the range seen in the data but keeps unobserved terms occurring between the first and last observed terms. Using `drop = "all"` removes all term levels that are not present in the data (may create 'gaps' in the term levels).  Using `drop = "none"` will keep all term levels between 2004SP and 2050Q2.
+#' @param Qs Logical.  Should quarters be included in the term levels? Overrides the `drop` argument.
+#'
+#' @return A factor with the term levels ordered to allow sorting.
+#' @export
+#'
+#' @examples
+factor_term = function(Term, drop = c("outside", "all", "none"), Qs = TRUE){
+
+	drop = match.arg(drop)
+
+	levels = paste0(rep(2004:2050, each = 7), c("SP", "Q3", "Q4", "SU", "FL", "Q1", "Q2"))
+
+	Term = factor(Term, levels = levels)
+
+	if(drop == "all"){
+		Term = droplevels(Term)
+	}
+
+	if(drop == "outside"){
+		first = as.character(sort(Term)[1])
+		start = which(levels == first)
+		last = as.character(sort(Term, decreasing = TRUE)[1])
+		end = which(levels == last)
+		levels = levels[start:end]
+		Term = factor(Term, levels = levels)
+	}
+
+	if(Qs == FALSE){
+		levels = levels(Term)
+		levels = levels[!grepl("Q", levels)]
+		Term = factor(Term, levels = levels)
+	}
+
+
+
+	Term
 }
